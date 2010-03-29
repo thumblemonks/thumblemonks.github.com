86d9f7bd9101e92653f64d23f5cc188dfc2ecd91
Justin Knowlden <gus@gusg.us>
2010-03-29 00:15:57 -0500

    Random ordering of members in collective

--- javascripts/thumblemonks.js
+++ javascripts/thumblemonks.js
@@ -1,4 +1,16 @@
+(function($) {
+  $.fn.randomize = function(randomizable) {
+    var that = $(this);
+    var randomly = function() { return (Math.round(Math.random()) - 0.25); };
+    return that.each(function() {
+      that.children(randomizable).remove().sort(randomly).each(function() { that.append(this); });
+    });
+  }
+})(jQuery);
+
 $(document).ready(function() {
+  $("#who ul").randomize("li");
+  
   $("#who li").click(function() {
     $("#who li.selected").removeClass("selected");
     $("#who .bio").hide();
